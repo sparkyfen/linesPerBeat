a067e6d378b347389cbcf284102720b5838c0e6e
brutalhonesty <sparky1010@gmail.com>
2014-07-08 22:18:42 -0700

    Updated shell script with color and more verbosity.

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
+green=$(tput setaf 2)
+reset=$(tput sgr0)
+echo -e "Copying Development Template..."
 cp lib/config/env/development.js.template lib/config/env/development.js
+echo -e "Copying Test Template..."
 cp lib/config/env/test.js.template lib/config/env/test.js
+echo -e "Copying Production Template..."
 cp lib/config/env/production.js.template lib/config/env/production.js
-echo "Don't forget to update the settings files within lib/config/env"
+echo -e "${green}Don't forget to update the settings files within lib/config/env.${reset}"
 node install.js

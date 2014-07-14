b26dd5ba6aa2885680acdf65b849c0a2847d71c2
brutalhonesty <sparky1010@gmail.com>
2014-07-13 19:31:05 -0700

    Added registration to API Docs.

--- docs/src/users.js
+++ docs/src/users.js
@@ -113,3 +113,68 @@
  *     HTTP/1.1 401 UnAuthorized
  *     {"message":"Please sign in."}
  */
+
+/**
+ * @api {post} /api/user/register Register
+ * @apiVersion 1.0.0
+ * @apiName Register
+ * @apiGroup User
+ * @apiPermission public
+ *
+ * @apiDescription Register the user onto the site.
+ *
+ * @apiParam {String} username The username of the account.
+ * @apiParam {String} password The password of the account.
+ * @apiParam {String} confirmPassword The confirmation password of the account.
+ * @apiParam {String} [firstName] The first name of the user making the account.
+ * @apiParam {String} [lastName] The last name of the user making the account.
+ *
+ * @apiExample CURL example:
+ *      curl -X POST 'http://example.com/api/user/register' -d 'username=foo&password=bar&confirmPassword=bar'
+ *
+ * @apiExample CURL example:
+ *      curl -X POST 'http://example.com/api/user/register' -d 'username=foo&password=bar&confirmPassword=bar&firstName=Mister&lastName=Bean'
+ *
+ * @apiSuccess {String} message The successful registration message.
+ *
+ * @apiSuccessExample Success-Response (example):
+ *     HTTP/1.1 200 OK
+ *     {"message":"Registered."}
+ *
+ * @apiError (Bad Request 400) MissingUsername The username was not in the request.
+ * @apiError (Bad Request 400) MissingPasswords The password or passwords were not in the request.
+ * @apiError (Bad Request 400) MismatchPasswords The passwords don't match.
+ * @apiError (Bad Request 400) UserIdAlreadyExists The user id already exists, the UUID has already been taken (should not happen).
+ * @apiError (Bad Request 400) AdminAccountFirst You need to make an administrative account first before a regular user.
+ * @apiError (Bad Request 400) UserAlreadyExists The username already exists.
+ * @apiError (Internal Server Error 500) ProblemRegistering There was an issue on the server serving the request.
+ *
+ * @apiErrorExample Error-Response (example):
+ *     HTTP/1.1 400 Bad Request
+ *     {"message":"Missing username."}
+ *
+ * @apiErrorExample Error-Response (example):
+ *     HTTP/1.1 400 Bad Request
+ *     {"message":"Missing password(s)."}
+ *
+ * @apiErrorExample Error-Response (example):
+ *     HTTP/1.1 400 Bad Request
+ *     {message: "Passwords don\'t match."}
+ *
+ * @apiErrorExample Error-Response (example):
+ *     HTTP/1.1 400 Bad Request
+ *     {message: "User id already exists."}
+ *
+ * @apiErrorExample Error-Response (example):
+ *     HTTP/1.1 400 Bad Request
+ *     {message: "You need to make an administrative account first."}
+ *
+ * @apiErrorExample Error-Response (example):
+ *     HTTP/1.1 400 Bad Request
+ *     {message: "User already exists."}
+ *
+ * @apiErrorExample Error-Response (example):
+ *     HTTP/1.1 500 Internal Server Error
+ *     {"message":"Problem registering user foo, please try again."}
+ *
+ */

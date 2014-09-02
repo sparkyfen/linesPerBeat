76b7c5b6a8c923e1ba256589a5eeb4da187bbba0
Adam Schodde <volvox@cloudy.dhcp.asu.edu>
2014-09-02 15:04:22 -0700

    Lowered karam wait time for webpage.

--- karma.conf.js
+++ karma.conf.js
@@ -51,6 +51,7 @@ module.exports = function(config) {
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
+    captureTimeout: 1000,
 
 
     // Start these browsers, currently available:
